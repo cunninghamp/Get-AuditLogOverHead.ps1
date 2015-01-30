f3d18c34ee2a3dbd363950a0afe5d42b5427631c
Paul Cunningham <paul@locklan.com.au>
2015-01-30 16:50:16 +1000

    V1.0 of script added

--- Get-AuditLogOverhead.ps1
+++ Get-AuditLogOverhead.ps1
@@ -1,28 +1,74 @@
-#<script-name>.ps1
-#Written By: <author>
-#
-#Change Log
-#V1.0, <date> - <comment>
-#
-#.SYNOPSIS 
-#Write a brief overview of the purpose of the script
-#
-#.EXAMPLE
-#Supply examples of how to use the script
-#
+<#
+.SYNOPSIS
+Get-AuditLogOverhead.ps1
+
+.DESCRIPTION 
+PowerShell script to report on the size of the mailbox audit logs stored in mailboxes in an Exchange Server organization.
+
+Use the ignorelist.txt file to specify any servers, DAGs, or databases you
+want the script to ignore (eg test/dev servers).
+
+.OUTPUTS
+Results are output to CSV file.
+
+.LINK
+http://exchangeserverpro.com/much-database-storage-mailbox-audit-logging-consume/
+
+.NOTES
+Written By: Paul Cunningham
+Website:	http://exchangeserverpro.com
+Twitter:	http://twitter.com/exchservpro
+
+Change Log
+V1.00, 30/01/2015 - Initial version
+#>
+
+#requires -version 2
 
 #...................................
 # Static Variables
 #...................................
 
-
-
-#...................................
-# Functions
-#...................................
-
+$report = @()
 
 
 #...................................
 # Script
 #...................................
+
+Write-Host "Retrieving mailboxes"
+$mailboxes = @(Get-Mailbox -Resultsize Unlimited)
+
+foreach ($mailbox in $mailboxes)
+{
+    $name = $mailbox.Name
+
+    Write-Host "Checking $name"
+
+    $auditsfolder = "$($mailbox.Identity)\Audits"
+
+    $foldersize = ($mailbox | Get-MailboxFolderStatistics -FolderScope RecoverableItems | Where {$_.Name -eq "Audits"}).FolderSize
+    if ($foldersize)
+    {
+        $foldersize = "{0:N2}" -f $foldersize.ToMB()
+    }
+    else
+    {
+        $foldersize = 0
+    }
+
+    $mailboxsize = (Get-MailboxStatistics $mailbox).TotalItemSize.Value.ToMB()
+
+    $reportObj = New-Object PSObject
+	$reportObj | Add-Member NoteProperty -Name "Name" -Value $name
+    $reportObj | Add-Member NoteProperty -Name "Mailbox Size (MB)" -Value $mailboxsize
+    $reportObj | Add-Member NoteProperty -Name "Audits Size (MB)" -Value $foldersize
+
+    $report += $reportObj
+
+    Write-Host "$name, $mailboxsize, $foldersize"
+}
+
+Write-Host "Writing output to AuditLogOverhead.csv"
+
+$report | Export-CSV AuditLogOverhead.csv -NoTypeInformation
