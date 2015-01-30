9395420c69330c120108df7fc7887954ad99863e
Paul Cunningham <paul@locklan.com.au>
2015-01-30 16:45:35 +1000

    New script added from template

---
+++ Get-AuditLogOverhead.ps1
@@ -0,0 +1,28 @@
+#<script-name>.ps1
+#Written By: <author>
+#
+#Change Log
+#V1.0, <date> - <comment>
+#
+#.SYNOPSIS 
+#Write a brief overview of the purpose of the script
+#
+#.EXAMPLE
+#Supply examples of how to use the script
+#
+
+#...................................
+# Static Variables
+#...................................
+
+
+
+#...................................
+# Functions
+#...................................
+
+
+
+#...................................
+# Script
+#...................................
